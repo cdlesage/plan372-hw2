8e7247da04c996a8ad39b2f1db743746a74b1a4f
cdlesage <cdl23@live.unc.edu>
2023-02-27 23:37:27 -0500

    Grouping data by inspector

--- plan372-hw2.R
+++ plan372-hw2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(lubridate)
 data = read_csv("restaurant_inspections.csv")
-#Histogram of all inspection scores
+#Histogram of all inspection scores (Q1)
 ggplot(data, aes(x=SCORE)) +
   geom_histogram(bins = 50) 
 
@@ -10,23 +10,28 @@ min(data$SCORE)
 which(data$SCORE == 0)
 
 # Plot of scores over time, showing that newer restaurants have higher scores
-# Answering Question 2
+# Q2
 ggplot(data, aes(x=DATE_, y=SCORE)) +
   geom_col()
 
-# Looking at how cities are written differently
+# Looking at how cities are written differently (Q3)
 unique(data$CITY)
 
-# Recoding city names 
+# Recoding city names (Q3)
 data$CITY = recode(data$CITY, "Raleigh"="RALEIGH", "FUQUAY-VARINA"="FUQUAY VARINA", "Fuquay Varina"="FUQUAY VARINA", "Fuquay-Varina"="FUQUAY VARINA",
                                "Apex"="APEX", "Zebulon"="ZEBULON", "Holly Springs"="HOLLY SPRINGS", "HOLLY SPRING"="HOLLY SPRINGS", "Cary"="CARY", "Morrisville"="MORRISVILLE", "MORRISVILE"="MORRISVILLE", "Garner"="GARNER")
 
-# Grouping data by city, averaging the inspection scores for each 
-cities = group_by(data, CITY, SCORE) %>%
+# Grouping data by city, averaging the inspection scores for each (Q3)
+cities = group_by(data, CITY) %>%
   summarize(
     SCORE=mean(SCORE)
   )
 
 
+# Grouping data by inspector (Q4)
+inspectors = group_by(data, INSPECTOR) %>%
+  summarize(
+    SCORE=mean(SCORE)
+  )
 
 
