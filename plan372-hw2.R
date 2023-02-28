755e33db839953c73de6a84330e06441f587362d
cdlesage <cdl23@live.unc.edu>
2023-02-27 23:23:33 -0500

    Grouping data by city

--- plan372-hw2.R
+++ plan372-hw2.R
@@ -10,5 +10,23 @@ min(data$SCORE)
 which(data$SCORE == 0)
 
 # Plot of scores over time, showing that newer restaurants have higher scores
+# Answering Question 2
 ggplot(data, aes(x=DATE_, y=SCORE)) +
   geom_col()
+
+# Looking at how cities are written differently
+unique(data$CITY)
+
+# Recoding city names 
+data$CITY = recode(data$CITY, "Raleigh"="RALEIGH", "FUQUAY-VARINA"="FUQUAY VARINA", "Fuquay Varina"="FUQUAY VARINA", "Fuquay-Varina"="FUQUAY VARINA",
+                               "Apex"="APEX", "Zebulon"="ZEBULON", "Holly Springs"="HOLLY SPRINGS", "HOLLY SPRING"="HOLLY SPRINGS", "Cary"="CARY", "Morrisville"="MORRISVILLE", "MORRISVILE"="MORRISVILLE", "Garner"="GARNER")
+
+# Grouping data by city, averaging the inspection scores for each 
+cities = group_by(data, CITY, SCORE) %>%
+  summarize(
+    SCORE=mean(SCORE)
+  )
+
+
+
+
